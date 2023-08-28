9554b24c755d149cc408f10041c06ad5c56de4e0
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-08-28 15:48:26 -0300

    par nome/valor

---
+++ parNomeValor.js
@@ -0,0 +1,23 @@
+//par Nome/Valor
+const saudacao = 'Opa' // contexto léxico 1
+
+function exec(){
+    const saudacao = 'Falaa'
+    return saudacao
+}
+const saudacao = 'Olá'
+//Objetos são grupos alinhados de pares nome/valor
+const cliente = {
+    nome:Jota,
+    idade:18,
+    Peso:66,
+    endereco: {
+       logradouro: 'Rua das alianças',
+       numero:77
+
+    }
+}
+
+console.log(saudacao)
+console.log(exec()
+)
