098127f0fc4b37ee86bc1fe8fb93e248e19c7865
Joao Victor Viana Marques <vjoaovictor746@gmail.com>
2023-08-29 14:17:43 -0300

    aritmeticos

---
+++ aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [4, 5, 8, 10]
+
+const soma = a + b + c + d
+const subtracao = d - c
+const multiplicacao = a * c
+const divisao = d / a 
+const mudulo = b % 2 
+
+console.log(soma, subtracao, multiplicacao, divisao, mudulo)
